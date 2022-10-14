160b7de484e22d4999e18a705a44ed3ef3de5bea
kptc.ru <kptc.ru@hotmail.com>
2022-10-14 08:36:50 +0300

    Для Коли

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.app2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.app2.classes.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Employee> employees = new ArrayList<>();
+    TextView name, surname, function;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editTextPersonName);
+        surname = findViewById(R.id.editTextPersonSurname);
+        function = findViewById(R.id.editTextPersonFunction);
+    }
+
+    public void addEmployee(View view){
+        Employee item = new Employee(name.getText().toString(), surname.getText().toString(), function.getText().toString());
+        employees.add(item);
+        if(employees.contains(item)){
+            System.out.println("Сотрудник добавлен");
+            Toast toast = Toast.makeText(this, "Сотрудник добавлен",Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 }
--- app/src/main/java/com/example/app2/classes/Employee.java
+++ app/src/main/java/com/example/app2/classes/Employee.java
@@ -2,5 +2,11 @@ package com.example.app2.classes;
 
 public class Employee {
 
-    private String name, surname;
+    private String name, surname, function;
+
+    public Employee(String name, String surname, String function) {
+        this.name = name;
+        this.surname = surname;
+        this.function = function;
+    }
 }
